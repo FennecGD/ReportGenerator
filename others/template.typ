d465daa7eace53e6c3dd01cf98f4e765bdc0e790
Ernest Gupik <ernestgupik@wp.pl>
2024-02-18 17:39:28 +0100

    Add report template + basic compile

---
+++ others/template.typ
@@ -0,0 +1,18 @@
+#set align(center)
+#set text(18pt)
+
+= {{ report_title }}
+{{ date }} \
+Prepared for: {{ prepared_for }}
+
+#set align(right)
+#set text(14pt)
+
+#block(height: 100pt)
+
+== Prepared by:
+{{ prepared_by }}
+
+#pagebreak()
+
+= Thing 1
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,35 @@
-use std::error::Error;
+use std::{
+    error::Error,
+    fs::{read_to_string, remove_file, OpenOptions},
+    io::Write,
+    process::Command,
+};
 
 mod args;
+
+const REPORT_FILE: &str = "report.pdf";
+const TMP_FILE: &str = "tmp.typ";
+
+fn compile_to_pdf(report: &str) {
+    // Write report to temporary file
+    let mut tmp_file = OpenOptions::new()
+        .write(true)
+        .create_new(true)
+        .open(TMP_FILE)
+        .expect("Failed to open temporary file");
+    tmp_file.write_all(report.as_bytes()).unwrap();
+    // Close file
+    drop(tmp_file);
+    // Use typst to compile the file
+    Command::new("typst")
+        .args(["compile", TMP_FILE, REPORT_FILE])
+        .spawn()
+        .expect("Failed to execute typst")
+        .wait()
+        .expect("Failed to wait for typst");
+    // Remove the temporary file
+    remove_file(TMP_FILE).expect("Failed to remove temporary file");
+}
 
 fn main() -> Result<(), Box<dyn Error>> {
     let args = args::get_args();
@@ -9,7 +38,19 @@ fn main() -> Result<(), Box<dyn Error>> {
     if let Some(command) = args.subcommand {
         match command.as_ref() {
             "new" => (),
-            "compile" => (),
+            "compile" => {
+                let mut report = include_str!("../others/template.typ").to_string();
+                let context: Vec<(&str, &str)> = vec![
+                    ("report_title", "Pentest Report"),
+                    ("date", "January 01, 2024"),
+                    ("prepared_for", "Example Data"),
+                    ("prepared_by", "Example Data"),
+                ];
+                for element in context {
+                    report = report.replace(&format!("{{{{ {} }}}}", &element.0), &element.1);
+                }
+                compile_to_pdf(&report);
+            }
             _ => {
                 eprintln!("Incorrect subcommand. Check --help");
                 std::process::exit(1);
