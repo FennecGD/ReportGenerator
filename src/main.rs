5b605a538bd02e38f89e7234255098b090b15218
Ernest Gupik <ernestgupik@wp.pl>
2024-03-01 00:17:59 +0100

    Code improvements

--- src/main.rs
+++ src/main.rs
@@ -3,23 +3,43 @@ use std::{
     error::Error,
     fs::{create_dir, remove_file, File, OpenOptions},
     io::{Read, Write},
+    path::PathBuf,
     process::{exit, Command},
 };
 
 mod args;
 
+/*
+   report
+   - metadata.txt (title, prepared_for, prepared_by, section_order (as TODO))
+   - sections (by default: summary.txt, methodology.txt, scope.txt)
+   - - section.txt (file name is section name by default (can overwrite with: title:newtitle in the first line), inside is the section content)
+   - findings
+   - - finding.txt (file name: finding name (ability to ovewrite the name), inside is the finding content + first lines ability to change things)
+*/
+
 const REPORT_FILE: &str = "report.pdf";
 const TMP_FILE: &str = "tmp.typ";
 const REPORT_TEMPLATE: &str = include_str!("../others/template.typ");
 
-fn compile_to_file(report: &str) {
+const EXAMPLE_METADATA: &str = "title:Example Pentest Report
+prepared_for:Example prepared for
+prepared_by:Example prepared by";
+
+const EXAMPLE_SECTION: &str = "title:Example section
+Look at this gorgeus sections content";
+
+const EXAMPLE_FINDING: &str = "title:Example finding
+Look at this amazing finding";
+
+fn compile_to_file(report: &str) -> Result<(), Box<dyn Error>> {
     // Write report to temporary file
     let mut tmp_file = OpenOptions::new()
         .write(true)
         .create_new(true)
         .open(TMP_FILE)
         .expect("Failed to open temporary file");
-    tmp_file.write_all(report.as_bytes()).unwrap();
+    tmp_file.write_all(report.as_bytes())?;
 
     // Close file
     drop(tmp_file);
@@ -34,6 +54,8 @@ fn compile_to_file(report: &str) {
 
     // Remove the temporary file
     remove_file(TMP_FILE).expect("Failed to remove temporary file");
+
+    Ok(())
 }
 
 fn get_current_date() -> String {
@@ -41,7 +63,7 @@ fn get_current_date() -> String {
     date.format("%B %d, %Y").to_string()
 }
 
-fn compile_report(report_dir: Option<std::path::PathBuf>) -> Result<(), Box<dyn Error>> {
+fn compile_report(report_dir: Option<PathBuf>) -> Result<(), Box<dyn Error>> {
     // Ensure user provided the report path
     let report_path = report_dir.unwrap_or_else(|| {
         eprintln!("ERROR: Report path not provided");
@@ -54,9 +76,9 @@ fn compile_report(report_dir: Option<std::path::PathBuf>) -> Result<(), Box<dyn 
         exit(1);
     }
 
-    let mut report_title = "Pentest Report";
-    let mut prepared_for = "Example Data";
-    let mut prepared_by = "Example Data";
+    let mut report_title = "CHANGE ME";
+    let mut prepared_for = "CHANGE ME";
+    let mut prepared_by = "CHANGE ME";
 
     let mut metadata = String::new();
     File::open(report_path.join("metadata.txt"))?.read_to_string(&mut metadata)?;
@@ -66,7 +88,6 @@ fn compile_report(report_dir: Option<std::path::PathBuf>) -> Result<(), Box<dyn 
         if split.len() < 2 {
             continue;
         }
-        println!("{split:?}");
         match split[0] {
             "title" => report_title = split[1],
             "prepared_for" => prepared_for = split[1],
@@ -92,11 +113,13 @@ fn compile_report(report_dir: Option<std::path::PathBuf>) -> Result<(), Box<dyn 
     for element in context {
         report = report.replace(&format!("{{{{ {} }}}}", element.0), element.1);
     }
-    compile_to_file(&report);
+
+    compile_to_file(&report)?;
+
     Ok(())
 }
 
-fn new_report(report_dir: Option<std::path::PathBuf>) -> Result<(), Box<dyn Error>> {
+fn new_report(report_dir: Option<PathBuf>) -> Result<(), Box<dyn Error>> {
     // Ensure user provided the report path
     let report_path = report_dir.unwrap_or_else(|| {
         eprintln!("ERROR: Report path not provided");
@@ -109,38 +132,19 @@ fn new_report(report_dir: Option<std::path::PathBuf>) -> Result<(), Box<dyn Erro
         exit(1);
     }
 
-    /*
-       report
-       - metadata.txt (title, prepared_for, prepared_by, section_order (as TODO))
-       - sections (by default: summary.txt, methodology.txt, scope.txt)
-       - - section.txt (file name is section name by default (can overwrite with: title:newtitle in the first line), inside is the section content)
-       - findings
-       - - finding.txt (file name: finding name (ability to ovewrite the name), inside is the finding content + first lines ability to change things)
-    */
-
     // Create the file structure
     create_dir(&report_path)?;
 
     let mut f_metadata = File::create_new(report_path.join("metadata.txt"))?;
-    f_metadata.write_all(
-        b"title:Example Pentest Report
-prepared_for:Example prepared for
-prepared_by:Example prepared by",
-    )?;
+    f_metadata.write_all(EXAMPLE_METADATA.as_bytes())?;
 
     create_dir(report_path.join("sections"))?;
     let mut f_section = File::create_new(report_path.join("sections").join("example_section.txt"))?;
-    f_section.write_all(
-        b"title:Example section
-Look at this gorgeus sections content",
-    )?;
+    f_section.write_all(EXAMPLE_SECTION.as_bytes())?;
 
     create_dir(report_path.join("findings"))?;
     let mut f_finding = File::create_new(report_path.join("findings").join("example_finding.txt"))?;
-    f_finding.write_all(
-        b"title:Example finding
-Look at this amazing finding",
-    )?;
+    f_finding.write_all(EXAMPLE_FINDING.as_bytes())?;
     Ok(())
 }
 
